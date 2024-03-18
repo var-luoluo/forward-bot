e00018716b8d3ea227f034b08b888fdcc7613c51
var-luoluo <var.name@outlook.com>
2024-03-18 20:53:20 +0800

    use run.log to record logging info

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import time
 import telebot
+import logging
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN') # export BOT_TOKEN='your_bot_token_from_@botfather'
 FROM_CHAT = 0 # chat_id, which chat to copy
@@ -12,9 +13,13 @@ stop_task_flag = False
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
+logging.basicConfig(filename='run.log', level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s')
+
 # Avoid abused
 @bot.message_handler(func=lambda message: message.from_user.id not in whitelist)
 def not_in_whitelist(message):
+    logging.info(f'user {message.chat.id} try to use bot ...')
     bot.send_message(message.chat.id, "抱歉，你没有权限使用该机器人。")
 
 @bot.message_handler(commands=['start'])
@@ -25,12 +30,14 @@ def start_info(message):
     /copy:  开始复制，请随指示操作
     /stop:  停止copy，由于未开启异步，意义不大
     '''
+    logging.info(f'user {message.chat.id} use /start ...')
     bot.reply_to(message, text, parse_mode="Markdown")
 
 @bot.message_handler(commands=['stop'])
 def stop_task(message):
     global stop_task_flag
     stop_task_flag = True
+    logging.info(f'user {message.chat.id} use /stop ...')
     bot.reply_to(message, "Task stopped.")
 
 # main function
@@ -39,26 +46,23 @@ def copy_handler(message):
     text = "请输入起始、结束消息id，用空格分开：\n(此部分暂未编写任何错误处理，请谨慎设置id)"
     send_msg = bot.send_message(
         message.chat.id, text, parse_mode="Markdown")
-    print(message.chat.id)
+    logging.info(f'user {message.chat.id} use /copy ...')
     bot.register_next_step_handler(send_msg, file_handler)
 
 def file_handler(message):
+    logging.info(f'with the text {message.text}')
     start_id, end_id = map(int, message.text.split())
     global stop_task_flag
     stop_task_flag = False
     bot.send_message(message.chat.id, "Copying ...", parse_mode="Markdown")
     for sign in range(start_id, end_id + 1):
-        if stop_task_flag:
-            text = f'目前已经复制完第{sign-1}条'
-            print(text)
-            bot.send_message(
-                message.chat.id, text, parse_mode="Markdown")
-            
+        if stop_task_flag:         
             break
         
         bot.copy_message(chat_id=TO_CHAT, from_chat_id=FROM_CHAT, message_id=sign)
         text = f'目前已经复制完第{sign}条'
         print(text)
+        logging.info(text)
         time.sleep(DELAY_TIME)
 
     if stop_task_flag:
@@ -66,6 +70,7 @@ def file_handler(message):
     else:
         text = f'任务已结束\n应复制 {end_id - start_id + 1 } 条消息\n实际复制 {sign - start_id + 1} 条消息'
     print(text)
+    logging.info(text)
 
 
     bot.send_message(message.chat.id, text, parse_mode="Markdown")
