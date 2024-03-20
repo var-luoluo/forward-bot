87b6d75f09c96759f0fc5f02e4e1c86a05e1b09f
var-luoluo <var.name@outlook.com>
2024-03-21 07:58:59 +0800

    support asynchronous by AsyncTelebot

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 import time
-import telebot
+# import telebot
 import logging
+from telebot.async_telebot import AsyncTelebot
 
 '''
 # If you can't set proxy outside, just use this with PySocks installed
@@ -22,19 +23,19 @@ whitelist = [] # user_id, who can use the bot
 stop_task_flag = False
 record = 0
 
-bot = telebot.TeleBot(BOT_TOKEN)
+bot = AsyncTelebot(BOT_TOKEN)
 
 logging.basicConfig(filename='run.log', level=logging.INFO,
     format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Avoid abused
 @bot.message_handler(func=lambda message: message.from_user.id not in whitelist)
-def not_in_whitelist(message):
+async def not_in_whitelist(message):
     logging.info(f'user {message.chat.id} try to use bot ...')
-    bot.send_message(message.chat.id, "抱歉，你没有权限使用该机器人。")
+    await bot.send_message(message.chat.id, "抱歉，你没有权限使用该机器人。")
 
 @bot.message_handler(commands=['start'])
-def start_info(message):
+async def start_info(message):
     text = '''Hi
     Bot功能未完善，错误处理羸弱，请谨慎使用
     /start:  欢迎命令
@@ -43,36 +44,36 @@ def start_info(message):
     /stop:  停止copy，由于未开启异步，意义不大
     '''
     logging.info(f'user {message.chat.id} use /start ...')
-    bot.reply_to(message, text, parse_mode="Markdown")
+    await bot.reply_to(message, text, parse_mode="Markdown")
 
 @bot.message_handler(commands=['stop'])
-def stop_task(message):
+async def stop_task(message):
     global stop_task_flag
     stop_task_flag = True
     logging.info(f'user {message.chat.id} use /stop ...')
-    bot.reply_to(message, "Task stopped.")
+    await bot.reply_to(message, "Task stopped.")
  
 @bot.message_handler(commands=['last'])
-def get_last_info(message):
+async def get_last_info(message):
     text = f'上一次copy的截止id是 {record}'
     logging.info(f'user {message.chat.id} use /last ...')
-    bot.send_message(message.chat.id, text)
+    await bot.send_message(message.chat.id, text)
 
 # main function
 @bot.message_handler(commands=['copy'])
-def copy_handler(message):
+async def copy_handler(message):
     text = "请输入起始、结束消息id，用空格分开：\n(此部分暂未编写任何错误处理，请谨慎设置id)"
     send_msg = bot.send_message(
         message.chat.id, text, parse_mode="Markdown")
     logging.info(f'user {message.chat.id} use /copy ...')
-    bot.register_next_step_handler(send_msg, file_handler)
+    await bot.register_next_step_handler(send_msg, file_handler)
 
-def file_handler(message):
+async def file_handler(message):
     logging.info(f'with the text {message.text}')
     start_id, end_id = map(int, message.text.split())
     global stop_task_flag
     stop_task_flag = False
-    bot.send_message(message.chat.id, "Copying ...", parse_mode="Markdown")
+    await bot.send_message(message.chat.id, "Copying ...", parse_mode="Markdown")
     for sign in range(start_id, end_id + 1):
         if stop_task_flag:         
             break
@@ -94,8 +95,9 @@ def file_handler(message):
     logging.info(text)
 
 
-    bot.send_message(message.chat.id, text, parse_mode="Markdown")
+    await bot.send_message(message.chat.id, text, parse_mode="Markdown")
     bot.clear_step_handler(message)
 
 
-bot.infinity_polling()
+import asyncio
+asyncio.run(bot.polling())
